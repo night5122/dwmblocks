c23fd1be48b4781f87806b41fbb05dc8b724941b
night5122 <night.5122@pm.me>
2021-05-31 23:06:57 -0500

    Pushing 0.0.1

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,12 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{""   , "dwmblocks-net", 2, 0},
-	{""   , "dwmblocks-vol", 2, 10},
-	{"  ", "dwmblocks-ram", 2, 0},
-	{"﬙  ", "dwmblocks-cpu", 2, 0},
-	{""   , "dwmblocks-bat", 2, 0},
-	{"  ", "dwmblocks-time", 5, 0},
-};
-
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+/*
+	_   ___       __    __  
+	/ | / (_)___ _/ /_  / /_    
+       /  |/ / / __ `/ __ \/ __/   
+      / /|  / / /_/ / / / / /_     
+     /_/ |_/_/\__, /_/ /_/\__/  
+             /____/    
+     Desktop Window Manager Blocks (0.0.1)
+*/
+static const Block blocks[] = {{""   , "dwmblocks-net", 2, 0},{""   , "dwmblocks-vol", 2, 10},{"  ", "dwmblocks-ram", 2, 0},{"﬙  ", "dwmblocks-cpu", 2, 0},{""   , "dwmblocks-bat", 2, 0},{"  ", "dwmblocks-time", 5, 0},};
 static char delim[] = "  ";
 static unsigned int delimLen = 5;
